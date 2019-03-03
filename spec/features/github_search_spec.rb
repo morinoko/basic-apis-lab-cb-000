bf539df83f7a96c72a095546550dde9c50b5b20f
Felice Forby <feliceforby@gmail.com>
2019-03-03 22:46:39 +0000

    Automatically backed up by Learn

--- spec/features/github_search_spec.rb
+++ spec/features/github_search_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe "Searching Github", type: :feature do
 
     expect(page).to have_link("https://github.com/dtrupenn/Tetris", href: "https://github.com/dtrupenn/Tetris")
     expect(page).to have_content("Tetris")
-    expect(page).to have_content("Use React, Redux, Immutable to code Tetris.")
+    expect(page).to have_content("A C implementation of Tetris using Pennsim through LC4")
   end
 end

80b9ae856c17ae31affbbb2f5d043cafe17638c9
Felice Forby <feliceforby@gmail.com>
2019-03-03 22:59:42 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'capybara'
   gem 'shoulda-matchers'
+  gem 'rails-controller-testing'
 end
 gem 'webmock', group: :test
 
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,7 +4,28 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    GITHUB_TOKEN = Rails.application.secrets.GITHUB_TOKEN
-    
+    client_id = Rails.application.secrets.github_client_id
+    client_secret = Rails.application.secrets.github_client_secret
+
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['client_id'] = client_id
+        req.params['client_secret'] = client_secret
+        req.params['q'] = params[:query]
+      end
+
+      body = JSON.parse(@resp.body)
+
+      if @resp.success?
+        @repositories = body['items']
+      else
+        @error = body['message']
+      end
+
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again"
+    end
+
+    render :search
   end
 end

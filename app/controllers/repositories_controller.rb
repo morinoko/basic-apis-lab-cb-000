6d70bea0c2e5ab30b6c9c9ad1caea7efd2db7e3d
Felice Forby <feliceforby@gmail.com>
2019-03-03 22:27:23 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,7 +4,24 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    github_token = Rails.application.secrets.GITHUB_TOKEN
+    github_token = Rails.application.secrets.github_token
+    client_id = Rails.application.secrets.github_client_id
+    client_secret = Rails.application.secrets.github_client_secret
 
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['q'] = params[:query]
+    end
+
+    body = JSON.parse(@resp.body)
+
+    if @resp.success?
+      @repositories = body['items']
+    else
+      #@error = body['items']
+    end
+
+    render :search
   end
 end

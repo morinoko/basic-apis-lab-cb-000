6f8caf535debf3d06d5ef2a2a998012431479ee4
Felice Forby <feliceforby@gmail.com>
2019-03-03 22:46:18 +0000

    Automatically backed up by Learn

--- spec/features/github_search_spec.rb
+++ spec/features/github_search_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe "Searching Github", type: :feature do
     fill_in 'query', with: "tetris"
     click_button "Search Github!"
 
-    expect(page).to have_link("react-tetris", href: "https://github.com/chvin/react-tetris")
-    expect(page).to have_content("react-tetris")
+    expect(page).to have_link("https://github.com/dtrupenn/Tetris", href: "https://github.com/dtrupenn/Tetris")
+    expect(page).to have_content("Tetris")
     expect(page).to have_content("Use React, Redux, Immutable to code Tetris.")
   end
 end
